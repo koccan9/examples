40aee8ea244d1be25fa1f0535b8e90ff02adb0eb
koccan9 <koccan9@gmail.com>
2020-03-20 01:28:19 +0300

    20.03.2020
    Factory Pattern Example2

---
+++ Factory Pattern Example2/Factory Pattern Example1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Factory_Pattern_Example1      //Creation OOP pattern.Factory Pattern
+{
+    interface IShape
+    {
+        void Draw();
+    }
+    class Triangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Triangle");
+        }
+    }
+    class Square : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Square");
+        }
+    }
+    enum ShapeType
+    {
+        Triangle, Square
+    }
+    interface IShapeFactory
+    {
+        IShape GetShape(ShapeType st);
+    }
+    class ConcreteShapeFactory : IShapeFactory
+    {
+        public IShape GetShape(ShapeType st)
+        {
+            if (st == ShapeType.Square)
+            {
+                return new Square();
+            }
+            return new Triangle();
+        }
+    }
+    static class FactoryTester//static test functions
+    {
+        private static IShapeFactory _shapeFactory;
+        static FactoryTester()//static constructor
+        {
+            _shapeFactory = new ConcreteShapeFactory();
+        }
+        public static void TestFactory()//test scenario1
+        {
+            IShape triangle = _shapeFactory.GetShape(ShapeType.Triangle);
+            IShape square = _shapeFactory.GetShape(ShapeType.Square);
+            triangle.Draw();
+            square.Draw();
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FactoryTester.TestFactory();
+        }
+    }
+}
