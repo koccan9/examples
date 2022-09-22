5c8521c56cb70bb11951f712735e3627750e7628
Can Koç <koccan9@outlook.com>
2022-09-22 21:02:37 +0300

    3

---
+++ Random Password Brute Force/Random Password Brute Force/Program.cs
@@ -0,0 +1,28 @@
+﻿static class Program
+{
+    const string letters = "asdfghjklşiçömnbvcxzqwertyuıopğü";
+    const int length = 4;
+    static void Main(string[] args)
+    {
+        var str = "";
+        var random = new Random();
+        for(var i = 0; i < length; i++)//we have generated our random password
+        {
+            str += letters[random.Next(0, letters.Length)];
+        }
+        Console.WriteLine("DEBUG for str: " + str);
+        var str2 = "";
+        for(int i = 0; i < str.Length; )//we are cracking the password with bruteforce that we created
+        {
+            char ch = letters[random.Next(0, letters.Length)];
+            if (ch == str[i])
+            {
+                i++;
+                str2 += ch;
+            }
+        }
+        Console.WriteLine("DEBUG for str2: " + str2);
+        Console.WriteLine(str + " == " + str2);
+        Console.ReadKey(true);
+    }
+}
---
+++ dictionary attack code basic/dictionary attack code basic/Program.cs
@@ -0,0 +1,23 @@
+﻿static class Program
+{
+    static void Main(string[] args)
+    {
+        if(args.Length != 2)
+        {
+            Console.WriteLine("wrong usage");
+            return;
+        }
+        FileStream fs = File.OpenRead(args[0]);
+        StreamReader sr = new StreamReader(fs);
+        var pass = args[1];
+        for(var i = sr.ReadLine(); i != null; i = sr.ReadLine())
+        {
+            if(pass == i)
+            {
+                Console.WriteLine("password is " + i);
+                return;
+            }
+        }
+        Console.WriteLine("password could not be found");
+    }
+}
